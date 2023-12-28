3c23382b8872c2e75fafb506d63a4e4808a86ce5
nickonsocialmedia <153699634+nickonsocialmedia@users.noreply.github.com>
2023-12-28 14:17:43 -0500

    Create script.js

---
+++ osi_Pizza/script.js
@@ -0,0 +1,13 @@
+/*const garbage-can = document.getElementById('garbage-can');
+const pizza-mobile = document.getElementById('pizza-mobile');
+
+
+function checkCollision() {
+  const garbage-canRect = getBoundingClientRect()'
+  const pizza-mobile = getBoundingClientRect();
+
+  if (
+    pizza-mobileRect.left <= garbage-can 
+)
+
+}
